9ff30969146f17d086d6a0cf32f87ecf8f4160d8
JaehoBuildiOSApp <jaeho.jung.01@gmail.com>
2022-11-08 05:35:16 +0900

    Initial Commit

---
+++ delegatePattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  delegatePattern
+//
+//  Created by Jaeho Jung on 2022/11/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
